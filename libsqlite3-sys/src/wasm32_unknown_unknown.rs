0dbdc88a86c28130fc7e6deb013b3559587f4eff
Gwo Tzu-Hsing <gotzehsing@gmail.com>
2024-11-29 20:29:42 +0800

    Align to 8 byte on wasm32

--- libsqlite3-sys/src/wasm32_unknown_unknown.rs
+++ libsqlite3-sys/src/wasm32_unknown_unknown.rs
@@ -35,14 +35,8 @@ pub unsafe extern "C" fn sqlite3_os_init() -> c_int {
     sqlite3_vfs_register(Box::leak(Box::new(vfs)), 1)
 }
 
-const fn max(a: usize, b: usize) -> usize {
-    [a, b][(a < b) as usize]
-}
-
-const ALIGN: usize = max(
-    8, // wasm32 max_align_t
-    max(std::mem::size_of::<usize>(), std::mem::align_of::<usize>()),
-);
+#[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
+const ALIGN: usize = 8;
 
 #[no_mangle]
 pub unsafe extern "C" fn malloc(size: usize) -> *mut u8 {
